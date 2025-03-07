0b27c80990eadb7b1c42bac2cfa1bc68ee913d67
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2025-03-08 00:38:23 +0530

    Fix route definition for getting job post by ID and add jobPost router to main router

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./user/user")
+const jobPostRouter = require("./jobPost/jobPost")
 
 
 router.use("/user", userRouter);
+
+router.use("/jobPost", jobPostRouter);
 
 module.exports = router;
