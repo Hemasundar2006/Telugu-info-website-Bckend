079c7ee5a3f19bf1c8be7a736e5dad3ce6f25126
Hemasundar2006 <marothihemasundar03@gmail.com>
2025-10-12 18:08:00 +0530

    Revert "Refactor server and index.js structure"
    This reverts commit 165bc0eae15c36b999a78b5227bd984429eceb03.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const mongoose = require('mongoose');
 const cors = require('cors');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
@@ -12,31 +12,8 @@ const io = socketIO(server, {
     }
 });
 
-// Middleware
-app.use(cors({
-    origin: ["https://telugu-info.vercel.app", process.env.FRONTEND_URL, "http://localhost:3000"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // Set io instance in app for use in routes
 app.set('io', io);
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// Import routes
-const feedbackRoutes = require('./src/routes/feedback');
-
-// Use routes
-app.use('/api/feedback', feedbackRoutes);
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
@@ -45,29 +22,6 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ 
-        success: false,
-        message: 'Something went wrong!',
-        error: err.message 
-    });
-});
-
-// Handle 404
-app.use((req, res) => {
-    res.status(404).json({ 
-        success: false,
-        message: 'Route not found' 
-    });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
